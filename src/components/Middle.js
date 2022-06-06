24b6e2893739ed7fc990756be67fc3eaeb381156
Nitish <78011049+FgrReloaded@users.noreply.github.com>
2022-06-06 14:21:25 +0530

    Update Middle.js

--- src/components/Middle.js
+++ src/components/Middle.js
@@ -6,7 +6,7 @@ function Middle() {
     <>
         <div className="middleContent">
             <div className='textCenter'>Installed over 10 lakhs star rated pumpsets across the country resulting in a cumulative saving of more than 9,000 million units of power for the nation.</div>
-            <img src='/3.png' alt="image" className='middleImage' />
+            <img src='/3.png' alt="reload" className='middleImage' />
             <p className='textCenter'>Vales - Pumps - Pipes - IoT Drives & Controllers - Wires & Cables - Solar Systems - Motors</p>
         </div>
     </>
